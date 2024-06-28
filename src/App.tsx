e05deb8a3c23a0a0e57c1b3cbafa1dcf43ec7fe2
fantomblackstar <mr.bravespirit@gmail.com>
2024-06-28 12:53:37 +0300

    Init eslint, prettier, tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="text-center text-3xl bg-blue-900">Text Converter</p>React
+      App
     </div>
   );
 }
