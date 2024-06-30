a0dfe06e54779189463c6593ce6616ee40de983d
fantomblackstar <mr.bravespirit@gmail.com>
2024-06-30 12:55:40 +0300

    Change readme.md, add small fixes

--- src/constants/layouts.ts
+++ src/constants/layouts.ts
@@ -2,5 +2,4 @@ import { HeaderNavItem } from "../types/layouts";
 
 export const HEADER_NAV_ITEMS: HeaderNavItem[] = [
   { href: "#history", title: "Історія" },
-  { href: "#about", title: "Про нас" },
 ];
